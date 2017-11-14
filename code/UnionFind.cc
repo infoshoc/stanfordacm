eb608cd54595afb13bf899201996c4c613b71824
Volodymyr Polosukhin <infoshoc@Volodymyrs-MacBook-Pro.local>
2017-11-15 00:01:03 +0200

    Union find with depth rank and path compression

--- code/UnionFind.cc
+++ code/UnionFind.cc
@@ -1,16 +1,46 @@
+// BEGIN CUT
 #include <iostream>
 #include <vector>
 using namespace std;
-int find(vector<int> &C, int x) { return (C[x] == x) ? x : C[x] = find(C, C[x]); }
-void merge(vector<int> &C, int x, int y) { C[find(C, x)] = find(C, y); }
+
+// END CUT
+struct UnionFind {
+    vector < int > parent;
+    vector < int > rank;
+    UnionFind(int n) : parent(n), rank(n) {
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+    int find_set (int v) {
+        if (v == parent[v])
+            return v;
+        return parent[v] = find_set (parent[v]);
+    }
+    void union_sets (int a, int b) {
+        a = find_set (a);
+        b = find_set (b);
+        if (a != b) {
+            if (rank[a] < rank[b])
+                swap (a, b);
+            parent[b] = a;
+            if (rank[a] == rank[b])
+                ++rank[a];
+        }
+    }
+};
+// BEGIN CUT
+
 int main()
 {
 	int n = 5;
-	vector<int> C(n);
-	for (int i = 0; i < n; i++) C[i] = i;
-	merge(C, 0, 2);
-	merge(C, 1, 0);
-	merge(C, 3, 4);
-	for (int i = 0; i < n; i++) cout << i << " " << find(C, i) << endl;
+	UnionFind C(n);
+	C.union_sets(0, 2);
+	C.union_sets(1, 0);
+	C.union_sets(3, 4);
+	for (int i = 0; i < n; i++) cout << i << " " << C.find_set(i) << endl;
 	return 0;
 }
+
+// END CUT
