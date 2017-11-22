4a903edd0fa905f45c72419adea5ae538e8a6c09
Infoshoc <infoshoc@infoshoc.infoshoc>
2017-11-22 13:59:02 +0200

    Treap and Ukkonen added

---
+++ code/Treap.cc
@@ -0,0 +1,55 @@
+struct item {
+    int key, prior;
+    int cnt;
+    item * l, * r;
+    item() { }
+    item (int key, int prior) : key(key), prior(prior), cnt(0), l(NULL), r(NULL) { }
+};
+typedef item * pitem;
+
+int cnt (pitem t) {
+    return t ? t->cnt : 0;
+}
+
+void upd_cnt (pitem t) {
+    if (t)
+        t->cnt = 1 + cnt(t->l) + cnt (t->r);
+}
+
+void merge (pitem & t, pitem l, pitem r) {
+    if (!l || !r)
+        t = l ? l : r;
+    else if (l->prior > r->prior)
+        merge (l->r, l->r, r),  t = l;
+    else
+        merge (r->l, l, r->l),  t = r;
+    upd_cnt (t);
+}
+
+void split (pitem t, pitem & l, pitem & r, int key, int add = 0) {
+    if (!t)
+        return void( l = r = 0 );
+    int cur_key = add + cnt(t->l);
+    if (key <= cur_key)
+        split (t->l, l, t->l, key, add),  r = t;
+    else
+        split (t->r, t->r, r, key, add + 1 + cnt(t->l)),  l = t;
+    upd_cnt (t);
+}
+
+void insert (pitem & t, pitem it) {
+    if (!t)
+        t = it;
+    else if (it->prior > t->prior)
+        split (t, it->l, it->r, it->key),  t = it;
+    else
+        insert (it->key < t->key ? t->l : t->r, it);
+}
+
+void erase (pitem & t, int key) {
+    if (t->key == key)
+        merge (t, t->l, t->r);
+    else
+        erase (key < t->key ? t->l : t->r, key);
+}
+
