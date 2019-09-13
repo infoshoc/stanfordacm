0179691f17592e1bff0a76c5398656cc35cca99e
infoshoc <infoshoc@infoshoc.infoshoc>
2019-09-13 22:55:00 +0300

    NTT from https://gist.github.com/math314/6a08301b8b75b8172798 added

---
+++ code/NTT.cc
@@ -0,0 +1,46 @@
+namespace NTT {
+	void _ntt(vector<ll>& a, int sign) {
+		const int n = sz(a);
+		assert((n ^ (n&-n)) == 0); //n = 2^k
+
+		const int g = 3; //g is primitive root of mod (g^(prime number <  mod) != 1)
+		int h = (int)mod_pow(g, (mod - 1) / n, mod); // h^n = 1
+		if (sign == -1) h = (int)mod_inv(h, mod); //h = h^-1 % 
+mod
+
+		//bit reverse
+		int i = 0;
+		for (int j = 1; j < n - 1; ++j) {
+			for (int k = n >> 1; k >(i ^= k); k >>= 1);
+			if (j < i) swap(a[i], a[j]);
+		}
+
+		for (int m = 1; m < n; m *= 2) {
+			const int m2 = 2 * m;
+			const ll base = mod_pow(h, n / m2, mod);
+			ll w = 1;
+			FOR(x, m) {
+				for (int s = x; s < n; s += m2) {
+					ll u = a[s];
+					ll d = a[s + m] * w % mod;
+					a[s] = u + d;
+					if (a[s] >= mod) a[s] -= mod;
+					a[s + m] = u - d;
+					if (a[s + m] < 0) a[s + m] += 
+mod;
+				}
+				w = w * base % mod;
+			}
+		}
+
+		for (auto& x : a) if (x < 0) x += mod;
+	}
+	void ntt(vector<ll>& input) {
+		_ntt(input, 1);
+	}
+	void intt(vector<ll>& input) {
+		_ntt(input, -1);
+		const int n_inv = mod_inv(sz(input), mod);
+		for (auto& x : input) x = x * n_inv % mod;
+	}
+}
